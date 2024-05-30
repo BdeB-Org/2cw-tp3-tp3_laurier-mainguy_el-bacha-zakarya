d53930df664a96ee3ff74c3a040e1bbceb289d94
Laurier <6228273@bdeb.qc.ca>
2024-05-30 17:31:28 -0400

    Implémentation du code javascript pour l'affichage des données de la base de données pour la table produit

--- site_web/javascript/produits_sport.js
+++ site_web/javascript/produits_sport.js
@@ -1,32 +1,38 @@
-function createNode(element)
-{
- return document.createElement(element);
+function createNode(element) {
+  return document.createElement(element);
 }
-function append(parent, el)
-{
-    return parent.appendChild(el);
+function append(parent, el) {
+  return parent.appendChild(el);
 }
-const emp_ul  = document.getElementById("produits");
-const url = "http://127.0.0.1:8080/ords/hr2/produits";
+function AfficherProduit()
+{
+//Constantes pour la table produit
+const produit_ul = document.getElementById("produits"); 
+const url = "http://localhost:8080/ords/hr2/produits/"; 
+//Fonction fetch pour Le transfert des données entre la base de données et le site
 fetch(url)
-  .then((resp) => resp.json())
-  .then(function (data) {
-    let produits = data.items; //.results;
-    return produits.map(function (produit) {
-      let li = createNode("li"),
-        span = createNode("span");
-      span.innerHTML = `${produit.nom_produit}`;
-      append(li, span);
-      append(emp_ul, li);
+    .then((resp) => resp.json())
+    .then(function (data) {
+        let produits = data.items;
+        return produits.map(function (produit) {
+            let li = createNode("li"),
+                span = createNode("span");
+            span.innerHTML = `Nom des Produits : ${produit.nom_produit}`;
+            append(li, span);
+            append(produit_ul, li);
+        });
+    })
+    //Un catch pour capturer si il y a une erreur dans l'exécution
+    .catch(function (error) {
+        console.log(JSON.stringify(error));
     });
-  })
-  .catch(function (error) {
-    console.log(JSON.stringify(error));
-  });
-    
+}
 
 
 
+
+
+  
 
 
 
