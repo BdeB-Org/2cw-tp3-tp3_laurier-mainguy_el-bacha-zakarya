c00f367ce24a5b5fc74dbf0e867aa0ba33902733
zakaryaelbacha <2385359@bdeb.qc.ca>
2024-05-30 22:11:18 -0400

    ajout du lien de la table de la base de données administration_aide dans aide javascript

---
+++ site_web/javascript/aide.js
@@ -0,0 +1,37 @@
+
+  function AfficherAide()
+  {
+ 
+  const elemC = document.getElementById("aide"); 
+  const url = "http://localhost:8080/ords/hr2/administration_aides/"; 
+
+
+  }
+  
+  
+  
+  
+  
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
