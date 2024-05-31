1da9db940a7f820666b3f7f400c7e51bec3c1f15
zakaryaelbacha <2385359@bdeb.qc.ca>
2024-05-30 22:38:35 -0400

    ajout du fetch dans aide javascript ainsi qu'un petit changement dans aide html

--- site_web/javascript/aide.js
+++ site_web/javascript/aide.js
@@ -1,11 +1,31 @@
 
+function createNode(element) {
+    return document.createElement(element);
+  }
+  function append(parent, el) {
+    return parent.appendChild(el);
+  }
   function AfficherAide()
   {
  
-  const elemC = document.getElementById("aide"); 
+  const elemA = document.getElementById("aides"); 
   const url = "http://localhost:8080/ords/hr2/administration_aides/"; 
 
-
+  fetch(url)
+      .then((resp) => resp.json())
+      .then(function (data) {
+          let aides= data.items;
+          return aides.map(function (aide) {
+              let li = createNode("li"),
+                  span = createNode("span");
+              span.innerHTML = `Prénom : ${aide.prenom_admin} ,Nom : ${aide.nom_admin} `;
+              append(li, span);
+              append(elemA, li);
+          });
+      })
+      .catch(function (error) {
+          console.log(JSON.stringify(error));
+      });
   }
   
   
